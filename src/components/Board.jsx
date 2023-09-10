2efbf58a844c5b694d90fc6f63c1c73f8a9c3c2a
Sebastian <mrseb10@gmail.com>
2023-09-10 17:54:28 +0200

    Win/Tie/Ongoing conditions

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,7 +8,9 @@ export default function Board(props) {
   const [board, setBoard] = React.useState(Array(9).fill(''));
   // Represents currentPlayer
   const [currentPlayer, setCurrentPlayer] = React.useState('x');
-  
+  // Represents game status
+  const [gameResult, setGameResult] = React.useState('ongoing');
+
   // function that switches turns
   function nextTurn() {
     setCurrentPlayer(prevPlayer => {
@@ -16,16 +18,40 @@ export default function Board(props) {
     })
   }
 
+  // board rendering component
   function BoardFields() {
     return board.map((cell, i) => (
       <div 
       key={i} 
-      className="board--field" 
+      className={`board--field field--${cell}`} 
       onClick={nextTurn}
       >{cell}</div>
     ))
   }
 
+  // function that checks result after each move
+  function checkWinner(board) {
+    const winConditions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (const condition of winConditions) {
+      const [a, b, c] = condition;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a]; // Return mark that is on this position
+      }
+
+      if (board.every(cell => cell !== '')) {
+        return 'tie' // If every cell is occupied and not found any win condition there is a tie
+      }
+
+      return null // Game is still ongoing
+    }
+  }
+
+  // function that check who's turn is it
   function whosTurn() {
     if (currentPlayer === 'x') {
       return xTurn
