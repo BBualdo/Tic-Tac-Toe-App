8abf3ea0458195d5ca120df94714d86baa02ac7d
Sebastian <mrseb10@gmail.com>
2023-09-10 14:50:54 +0200

    Choosing the Mark

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import Menu from "./components/Menu";
 import Board from "./components/Board";
+import { render } from "react-dom";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = React.useState(
-  <Menu 
-    togglePage={togglePage}
-  />)
+  const [player1, setPlayer1] = React.useState('X');
+  const [currentPage, setCurrentPage] = React.useState('menu');
 
+// function to change player related on mark
+  function chooseMark(mark) {
+    setPlayer1(mark);
+  }
+
+// function to change the page to board
   function togglePage() {
-    setCurrentPage(<Board />)
+    setCurrentPage('board')
+  }
+// function to render the page with updated props
+  function renderPage() {
+    if (currentPage === 'menu') {
+      return (
+        <Menu 
+          togglePage={togglePage}
+          chooseMark={chooseMark}
+          player={player1}
+        />
+      )
+    } else {
+      return <Board />
+    }
   }
 
   return (
     <main>
-      {currentPage}
+      {renderPage()}
     </main>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import logo from '../images/logo.svg';
-
+import pickX from '../images/pick-x.svg'
+import pickO from '../images/pick-o.svg'
 export default function Menu(props) {
+  
   return (
     <section className="menu">
       <img className="app--logo" src={logo}/>
       <div className="pick-mark">
         <h4>Pick Player 1's Mark</h4>
         <div className="mark-picker">
-          <div className="pick--x"/>
-          <div className="pick--o"/>
+          <div 
+          onClick={() => props.chooseMark('X')}
+          className={`pick--x 
+          ${props.player === 'X'
+          ? 'picked'
+          : 'not-picked'}`}
+          >
+            <img src={pickX}/>
+          </div>
+          <div 
+          onClick={() => props.chooseMark('O')}
+          className={`pick--o 
+          ${props.player === 'O'
+          ? 'picked'
+          : 'not-picked'}`}
+          >
+            <img src={pickO}/>
+          </div>
         </div>
         <p className="hint">Remember : X Goes First</p>
       </div>
